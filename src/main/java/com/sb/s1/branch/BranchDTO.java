9d9cabc6c4fae08e75f02104c0a40002df1977a1
이석준 <llddoo@naver.com>
2021-04-12 18:11:10 +0900

    04.12 branch 시작

--- src/main/java/com/sb/s1/branch/BranchDTO.java
+++ src/main/java/com/sb/s1/branch/BranchDTO.java
@@ -2,4 +2,28 @@ package com.sb.s1.branch;
 
 public class BranchDTO {
 
+	private long branchNum;
+	private String branchName;
+	private String branchCall;
+	
+	public long getBranchNum() {
+		return branchNum;
+	}
+	public void setBranchNum(long branchNum) {
+		this.branchNum = branchNum;
+	}
+	public String getBranchName() {
+		return branchName;
+	}
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+	public String getBranchCall() {
+		return branchCall;
+	}
+	public void setBranchCall(String branchCall) {
+		this.branchCall = branchCall;
+	}
+	
+	
 }
