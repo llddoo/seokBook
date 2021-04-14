2b49d62338bf33dbcecab1f6e2b0856a66bdc1a2
llddoo <74532161+llddoo@users.noreply.github.com>
2021-04-14 17:52:23 +0900

    dd

--- src/main/java/com/sb/s1/branch/BranchController.java
+++ src/main/java/com/sb/s1/branch/BranchController.java
@@ -2,6 +2,8 @@ package com.sb.s1.branch;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,10 +15,14 @@ public class BranchController {
 	private BranchService branchService;
 	
 	@RequestMapping("branchInsert")
-	public ModelAndView setInsert() throws Exception {
-		ModelAndView mv = new ModelAndView();
+	public int setInsert(BranchDTO branchDTO, Model model) throws Exception {
 		
-		return mv;
+		int result = branchService.setInsert(branchDTO);
+		model.addAttribute("dto", branchDTO);
+		
+		return result;
+		
+		
 	}
 	
 }
