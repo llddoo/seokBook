8c7675c793e4d84acbc50d4c0ed15e0eba826eda
vkfkd420 <80494509+vkfkd420@users.noreply.github.com>
2021-04-23 12:35:49 +0900

    Update memberFindID.js

--- src/main/webapp/resources/js/member/memberFindID.js
+++ src/main/webapp/resources/js/member/memberFindID.js
@@ -2,7 +2,7 @@
  * 
  */
 let email = document.getElementById("email");
-let find = document.getElementById("find");
+
 btn.addEventListener("click", function(){
 	for(let e of etc){
 		if(e.value == ""){
@@ -13,7 +13,7 @@ btn.addEventListener("click", function(){
 	
 	//조건이 만족하면
 	if(idCheckResult && pwCheckResult && pwEqualResult && etcResult){
-
+		let find = document.getElementById("find");
 		//frm.submit();
 		alert("11111111111");
 	}else {
